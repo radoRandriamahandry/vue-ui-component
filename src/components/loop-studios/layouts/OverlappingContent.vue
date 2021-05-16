51ee9d2ed9720210114276247dcf2dfe15161d56
radoRandriamahandry <61495462+radoRandriamahandry@users.noreply.github.com>
2021-05-16 15:18:56 +0300

    mobile: finish mobile version for first section

--- src/components/loop-studios/layouts/OverlappingContent.vue
+++ src/components/loop-studios/layouts/OverlappingContent.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="grid grid-cols-9 grid-rows-4">
+  <div class="hidden grid-cols-9 grid-rows-4 sm:grid">
     <div class="col-start-1 col-end-6 row-start-1 row-end-4">
       <slot name="firstContent"></slot>
     </div>
     <div class="col-start-5 col-end-10 row-start-2 row-end-5">
+      <slot name="secondContent"></slot>
+    </div>
+  </div>
+  <div class="grid sm:hidden">
+    <div class="">
+      <slot name="firstContent"></slot>
+    </div>
+    <div class="">
       <slot name="secondContent"></slot>
     </div>
   </div>
