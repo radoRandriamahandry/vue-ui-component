c1d5779f7a116811e1020792a6d49cea7f817638
radoRandriamahandry <61495462+radoRandriamahandry@users.noreply.github.com>
2021-05-17 22:28:24 +0300

    feat: finish grid layout for all the section

--- src/components/blogr/layouts/HeroBackground.vue
+++ src/components/blogr/layouts/HeroBackground.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="relative h-full">
+  <!-- <div class="relative h-full">
+    <div
+      class="absolute w-full h-full bg-gradient-to-r from-blogr-lighter-red to-blogr-light-red"
+    ></div>
+    <img
+      class="absolute bg-grad min-w-min -left-1/4"
+      style="top: -1300px"
+      src="/src/assets/LPageBlogr/bg-pattern-intro.svg"
+      alt="background image"
+    />
+  </div> -->
+  <div class="relative flex items-center h-full">
     <div
       class="absolute w-full h-full  bg-gradient-to-r from-blogr-lighter-red to-blogr-light-red"
     ></div>
     <img
       class="absolute bg-grad min-w-min -left-1/4"
-      style="top: -1300px"
       src="/src/assets/LPageBlogr/bg-pattern-intro.svg"
       alt="background image"
     />
