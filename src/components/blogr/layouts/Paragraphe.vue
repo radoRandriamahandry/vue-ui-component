6628fd74b8194620b7a34c43f8951efcb72c19e6
radoRandriamahandry <61495462+radoRandriamahandry@users.noreply.github.com>
2021-05-18 12:00:02 +0300

    feat: make the Paragraphe component dynamic - change white the background (props) value

--- src/components/blogr/layouts/Paragraphe.vue
+++ src/components/blogr/layouts/Paragraphe.vue
@@ -1,10 +1,37 @@
 <template>
   <div class="pr-28">
-    <h3 class="text-xl text-blogr-primary-blue">
+    <h3
+      class="text-xl font-semibold"
+      :class="[
+        background == 'light' ? 'text-blogr-primary-blue' : 'text-white',
+      ]"
+    >
       <slot name="title"></slot>
     </h3>
-    <p class="mt-5">
+    <p
+      class="mt-5"
+      :class="[
+        background == 'light'
+          ? 'text-blogr-neutral-dark-gray-blue'
+          : 'text-white',
+      ]"
+    >
       <slot name="content"></slot>
     </p>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    background: {
+      type: String,
+      default: "light",
+      required: false,
+      validator: (value) => {
+        return ["light", "dark"].indexOf(value) !== -1
+      },
+    },
+  },
+}
+</script>
